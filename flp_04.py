9429c2a1dd00a1190f78a12001b8d11a0bf3b38e
Paul Zierep <paul.zierep@pharmazie.uni-freiburg.de>
2017-03-13 16:21:48 +0100

    reduction to the really needed function

--- flp_04.py
+++ flp_04.py
@@ -103,10 +103,9 @@ class pks_mol():
 
 	def path_recursion(self, no_peptide = True, **kwargs):
 
-		'''this function basically does the same as the rdkit function FindAllPathsOfLengthN(), but
-		it only gives the unique long paths instead of all paths of the length N, this will hopefully improve 
-		the time management, as only a very limited number of paths is directly calculated by a very straight forward approach, 
-		instead of first calculation all the paths and then setting conditions...''' 
+		'''this function has a more specific design as the rdkit FindAllPathsOfLengthN() function, but
+		it only gives the unique longest paths instead of all paths of the length N, it is also reasonable fast due to the 
+		recursive implemention. Still large ring systems can take w long time to process.''' 
 
 		def list_change(liste):
 			'''changes a list with direction marker (+) to a normal list, which is needed for plotting or check up'''
@@ -124,7 +123,6 @@ class pks_mol():
 			unique_paths = []	#for every run the unique paths which can not be further explored are stored in this list.
 			new_dics = {}		# this dic will contain the last chain atom as key and the already explored path as value.
 
-			
 			for k in dics:	# go through the atoms
 
 				#ring system, back conversion
@@ -184,7 +182,7 @@ class pks_mol():
 						as the valance for C atoms is max. 4, this three special cases are enough to take care of that matter, therefore even very complex cyclic systems should 
 						be covered. The system was tested with the following smiles: C(=O)CCCC12(CC(C)(C2)C1), which yielded in all possible unique paths''' 
 						if n.GetIdx() not in list_change(dics[k]): # (*) check if the neighbor was already in the old_atoms list
-							if n.GetIdx() in new_dics.keys():	#here the possibility is checked, that both paths connect, which
+							if n.GetIdx() in new_dics.keys():	#here the possibility is checked, that both paths connect
 								if (n.GetIdx(),'+') in new_dics.keys():
 									if (n.GetIdx(),'++') in new_dics.keys():
 										new_list = dics[k] + [k] #concatenate start atom to the old atom list
@@ -216,6 +214,7 @@ class pks_mol():
 			return(new_dics, unique_paths)
 
 
+		#Pos 1
 		start = self.find_starting_atom()	#get a list of start indices, to initiate the recursive function
 
 		longest_paths = {}	# this will be the dic where for each starting atom, a list of lists will be stored which contains 
@@ -336,14 +335,13 @@ class pks_mol():
 					neighbor_list.append(neighbor_list_it)
 
 			matrix[index] = [current_atom.GetSymbol(), bond, neighbor_list]
-		# print matrix
-		# 	#exit()
-		# 		#print matrix
+
+
 		side_atom_idx = list(set(side_atom_idx))
 		return(matrix, side_atom_idx)
 
 	def get_pks_paths(self, path_list):
-		'''produces for all the paths the matrices'''
+		'''produces for all the paths the path objects'''
 		pks_path_list = []
 		#print 'hi'
 		for key in path_list:
@@ -359,229 +357,7 @@ class pks_mol():
 		#matrix_dic = self.matrix_self_filter_02(matrix_dic) #for me it seems reasonable to apply this filter all the time !!
 		return(pks_path_list)
 
-
-	def matrix_self_filter_01(self, matrix_list):
-		'''makes sure the same matrix is not multiple times created, could be the case for ring system'''
-		for matrix in matrix_list:
-			new_matrix_list = matrix_list
-			counter = matrix_list.count(matrix)
-			while counter > 1:
-				new_matrix_list.remove(matrix)
-				counter -= 1 
-
-		return(matrix_list)
-
-	def matrix_self_filter_02(self, matrix_dic):
-		'''makes sure there are no matrices, that code for the same smiles'''
-		smiles_dict = {} 
-
-		for matrix in matrix_dic:
-			mol, main_path = streight_path(matrix)
-			smiles = Chem.MolToSmiles(mol, canonical=True)
-			#print smiles
-			smiles_dict[smiles] = matrix 
-
-		# for key in smiles_dict:
-		# 	print key
-		matrix_list = smiles_dict.values() #as the smiles is the key, there can only be one value each. 
-
-		return(matrix_list)
-
-
-	def matrix_match(self, matrix):
-		mol = self.mol
-	#index = 0
-	#for matrix in matrix_list:
-		#if index < 10:
-		matrix_mol, main_path = streight_path(matrix)
-		mol_list = [mol, matrix_mol]
-		comp = rdFMCS.FindMCS(mol_list)
-		smart_match = comp.smartsString
-		atom_match = comp.numAtoms
-		bond_match = comp.numBonds
-		Match = mol.GetSubstructMatches(Chem.MolFromSmarts(smart_match), useChirality=False)
-
-		# img = draw_mol_and_path(smi1.mol, list(Match[0]))
-		# img.show()	
-		# 	#Draw.MolToImage(main_path).show()
-		# index += 1
-		return(list(Match[0]), atom_match, bond_match)
-
-
-	# def matrix_self_filter_02(self, matrix_list, min_length):
-	# 	'''makes sure the same matrix is not multiple times created, could be the case for ring systems'''
-	# 	for matrix in matrix_list:
-	# 		new_matrix_list = matrix_list
-	# 		counter = matrix_list.count(matrix)
-	# 		while counter > 1:
-	# 			new_matrix_list.remove(matrix)
-	# 			counter -= 1 
-
-	# 	return(matrix_list)
-
-
-
-
-
 '''independent functions'''
-
-def pks_paths_only_matrix(pks_path_list):
-	'''extracts the matrices from the pks_path_list, which are needed for the scoring_algo'''
-	matrix_list = []
-	for k in pks_path_list:
-		matrix_list.append(k.matrix)
-
-	return(matrix_list)
-
-
-def no_sub_smiles(smiles_list_comp):
-	'''takes a list of smiles and compares them with each other, when one smile is a subsmile to a bigger smile in the list, the small smiles will be deleted. 
-	Subsequently only main smiles remain which are not subsmiles themselves, Example:
-	List_init = [CCC(C), CCC, CC(O)C] ==> List_after = [CCC(C), CC(O)C]'''
-
-	unique_smiles = []
-	for smiles in smiles_list_comp:
-		copy = smiles_list_comp
-		#copy.remove(smiles)
-		mol1 = Chem.MolFromSmiles(smiles)
-		match_count = 0
-		#print copy
-		for smi in copy:
-			mol2 = Chem.MolFromSmiles(smi)
-			comp_liste = [mol1, mol2] 
-			comp = rdFMCS.FindMCS(comp_liste)
-			smart_match = comp.smartsString
-			comp_match = Chem.MolToSmarts(Chem.MolFromSmiles(smiles))
-			if comp_match == smart_match:
-				match_count += 1 #the first match is the compound itself, if it finds another match besides itself, it is not the biggest unique path !!
-
-		if match_count == 1:
-			unique_smiles.append(Chem.MolToSmiles(Chem.MolFromSmiles(smiles)))
-
-	unique_smiles = list(set(unique_smiles))
-	return(unique_smiles)
-
-
-def streight_path(dic_path):
-	'''Takes a path in the matrix format and creates a mol object, like the inverse of the smile2matrix program, but side paths which would be involved in a ring for example 
-	get shown in the way the comparison algo looks at it , ex.: 
-		C-O-C		smile2matrix														streight_path		  O-C   C-O
-		|	|																									|	|
-	C-C-C-C-C-C-C		---->		{0: ['C', 'SINGLE', []], 1: ['C', 'SINGLE' []]...... 	---->			C-C-C-C-C-C-C
-
-	Additionally the mol object is linearised, meaning, that the main_path is taken as a straight template and the entire path is oriented the same way:
-	See.: http://www.rdkit.org/docs/GettingStartedInPython.html; Working with 2D molecules: Generating Depictions
-	'''
-
-	#dic_path = ast.literal_eval(str(matrix))
-
-	for key in dic_path:
-		# print key
-
-		if int(key) == 0:
-			last_atom_idx = 0
-			last_atom_idx2 = 0
-			molecule = Chem.MolFromSmiles(str(dic_path[key][0]))
-			molecule = Chem.RWMol(molecule)
-			main_path = Chem.RWMol(molecule)
-			#print molecule.GetAtomWithIdx(0).GetIdx()
-			# first atom index = 0
-
-			#check side paths
-			if dic_path[key][2] != []:
-				side_paths = dic_path[key][2]
-				#print side_paths
-				for path in side_paths:
-					k = molecule.AddAtom(Chem.Atom(str(path[1])))
-					molecule.AddBond(0, k, rdkit_bond_from_str(str(path[0])))
-					if path[2] != []:
-						#print 'k'
-						side_sidepath = path[2]
-						for path2 in side_sidepath:
-							k2 =  molecule.AddAtom(Chem.Atom(str(path2[1])))
-							molecule.AddBond(k, k2, rdkit_bond_from_str(str(path2[0])))
-
-
-
-		else:
-			index_atom = molecule.AddAtom(Chem.Atom(str(dic_path[key][0])))
-			molecule.AddBond(last_atom_idx, index_atom, rdkit_bond_from_str(str(dic_path[int(key)-1][1])))
-
-			index_atom2 = main_path.AddAtom(Chem.Atom(str(dic_path[key][0])))
-			# print last_atom_idx
-			# print index_atom2
-			# exit()
-			main_path.AddBond(last_atom_idx2, index_atom2, rdkit_bond_from_str(str(dic_path[int(key)-1][1]))) 
-
-
-			if dic_path[key][2] != []:
-				side_paths = dic_path[key][2]
-				#print side_paths
-				for path in side_paths:
-					k = molecule.AddAtom(Chem.Atom(str(path[1])))
-					molecule.AddBond(index_atom, k, rdkit_bond_from_str(str(path[0])))
-					if path[2] != []:
-						#print 'k'
-						side_sidepath = path[2]
-						for path2 in side_sidepath:
-							k2 =  molecule.AddAtom(Chem.Atom(str(path2[1])))
-							molecule.AddBond(k, k2, rdkit_bond_from_str(str(path2[0])))
-
-			last_atom_idx = index_atom
-			last_atom_idx2 = index_atom2
-
-	
-	mol =  Chem.RemoveHs(molecule.GetMol())
-	main_path = Chem.RemoveHs(main_path.GetMol())
-	AllChem.Compute2DCoords(main_path)
-	AllChem.GenerateDepictionMatching2DStructure(mol ,main_path)
-
-	return(mol, main_path)
-
-
-def streight_path_all(liste, subsmiles = None):
-	mol_list = []
-	for elem in liste:
-		mol, s_path = streight_path(elem)
-		#smiles = Chem.MolToSmiles(mol)
-		mol_list.append(mol)
-
-	#if subsmiles == None:
-	#smiles_list = no_sub_smiles(smiles_list)
-		
-	return(mol_list)
-
-def streight_path_all_mol(liste):
-	mol_list = []
-	for elem in liste:
-		mol, s_path = streight_path(elem)
-		#smiles = Chem.MolToSmiles(mol)
-		mol_list.append(mol)
-	return(mol_list)
-
-
-def rdkit_bond_from_str(string):
-	'''as the Chem.BondTypes can not be initialized in python, see rdkit website, http://www.rdkit.org/Python_Docs/rdkit.Chem.rdchem.Bond-class.html:
-				__init__(...)
-			(Constructor)
-				 
-
-			Raises an exception
-			This class cannot be instantiated from Python
-
-			Overrides: object.__init__
-		this little program helps to create them anyway !! More bond types can be added here, but for my purpose these where enough.'''
-
-	if string == 'SINGLE':
-		bondtype = Chem.BondType.SINGLE
-	if string == 'DOUBLE':
-		bondtype = Chem.BondType.DOUBLE
-	if string == 'AROMATIC':
-		bondtype = Chem.BondType.UNSPECIFIED #no idea what to do with the aromatic bonds at the moment 
-	if string == 'TRIPLE':
-		bondtype = Chem.BondType.TRIPLE
-	return(bondtype)
-
 
 def draw_mol_and_path(mol, path):
 
@@ -625,116 +401,52 @@ def draw_mol_and_path_save(mol, path, file_path):
 	# print color_dict
 		index += 1
 
-	# 	# print color_dict
-	# 	index += 1
-	# for atom in side_atom_idx:
-	# 	color_dict[atom] = [0,0,1]
-
-		#atom_list.append(atom)
-	#print color_dict
-	# print mol
-	# print color_dict
-
 	Draw.MolToFile(mol, file_path, size=(600,600), highlightMap = color_dict, type = 'svg')
 
 
-def list_change(liste):
-	'''changes a list with direction marker (+) to a normal list, which is needed for plotting or check up'''
-	news = []
-	for k in liste:
-		if type(k) == type(()):
-			k = k[0]
-		news.append(k)
-	return(news)
 
-def atom_from_smiles(smiles):
-	'''creates an rdkit atom object with idx. 0 from a one latter smiles'''
-	mol1 = Chem.MolFromSmiles(smiles)
-	atom1 = mol1.GetAtomWithIdx(0)
-	return(atom1)
+##debug and explanatio### 
 
-def list2mol(mol, liste):
-	'''takes a mol object and a list with atom indices contained in the mol object and creates a mol for only the list atoms'''
-	atom_list = []
-	bond_list = []
-	
-	index = 0
-	for num in liste:
-		current_atom = mol.GetAtomWithIdx(liste[index])
-		if index > 0:
-			last_atom = mol.GetAtomWithIdx(liste[index-1])
-			bond_list.append(mol.GetBondBetweenAtoms(current_atom.GetIdx(), last_atom.GetIdx()).GetBondType())
-		index += 1
-		atom_list.append(current_atom.GetSymbol())
+# #uncomment step by step to learn more about the class.
+# #all the major functionalists are demonstrated.
 
-	'''init the RWMol object'''
-	mol = Chem.Mol()
-	emol = Chem.RWMol(mol)
+# smiles = 'CO[C@H]1C[C@H](O[C@H]2[C@@H](C)/C=C/C=C/3\\CC(CCC=O)C(CC=O)CO[C@H]4[C@]3(O)[C@@H](C=C([C@H]4O)C)C(=O)O[C@H]3C[C@@H](C/C=C/2\\C)O[C@]2(C3)C=C[C@@H]([C@H](O2)[C@H](CC)C)C)O[C@H]([C@@H]1O[C@H]1C[C@H](OC)[C@H]([C@@H](O1)C)O)C'
 
-	'''fill RWMol object with atoms and bonds and save the idx in a dic for later'''
-	atom_dic = {}
-
-	#get the atoms
-	for ato in atom_list:
-		ato = atom_from_smiles(ato)
-		idx1 = emol.AddAtom(ato)
-		atom_dic[idx1] = ato
-	#get the bonds
-	index = 0
-	for bon in bond_list:
-
-		emol.AddBond(index, (index + 1), bon)
-		index += 1
-
-	mol = emol.GetMol()
-	return(mol, atom_dic)
-
-
-###debug### 
-
-#uncomment step by step to learn more about the class.
-#all the major functionalists are demonstrated.
-
-smiles = 'CO[C@H]1C[C@H](O[C@H]2[C@@H](C)/C=C/C=C/3\\CC(CCC=O)C(CC=O)CO[C@H]4[C@]3(O)[C@@H](C=C([C@H]4O)C)C(=O)O[C@H]3C[C@@H](C/C=C/2\\C)O[C@]2(C3)C=C[C@@H]([C@H](O2)[C@H](CC)C)C)O[C@H]([C@@H]1O[C@H]1C[C@H](OC)[C@H]([C@@H](O1)C)O)C'
-
-#initiate the object
-object1 = pks_mol(smiles)
-
-object1.draw_and_show(size = (500,500))
-
-print object1.get_starting_matches()
-print object1.find_starting_atom()
-k = object1.path_recursion(exclude = ['O','S'])
-for key in k:
-	print key
-
-draw_mol_and_path(object1.mol, k[17][-1]).show()
-draw_mol_and_path(object1.mol, k[34][-1]).show()
-draw_mol_and_path(object1.mol, k[21][-1]).show()
+# #initiate the object
+# object1 = pks_mol(smiles)
 
 # #show it
-# object1.draw_and_show()
+# object1.draw_and_show(size = (500,500))
 
-# #all the paths with starting idx as key (O and S are not in the path):
+# # starting units 
+# print object1.get_starting_matches()
+
+# # starting atoms
+# print object1.find_starting_atom()
+
+# # all paths for this object (O and S are not in the path)
 # k = object1.path_recursion(exclude = ['O','S'])
+# # print k
 
-# #create a figure with the path inside, can also be donw with iteration
-# img_with_path = draw_mol_and_path(object1.mol, k[24][-1])
+# # # draw an example
+# for key in k:
+# 	for lis in k[key]:
+# 		print lis
+# 		draw_mol_and_path(object1.mol, lis).show()
+# 	break	
+	
 
-# #show the figure
-# img_with_path.show()
-
-# #matrix, returns the matrix description of a path which can be used to compare paths to each other (like a fingerprint), the 
-# #documentation for the comparison synthax and the matrix will follow soon,
-# #also the side indices will be returned in order to allow to color them different for example
-# matrix, side_idx = object1.path2matrix_3(k[24][-1])
+# # #matrix, returns the matrix description of a path which can be used to compare paths to each other (like a fingerprint), the 
+# # #documentation for the comparison synthax and the matrix will follow soon,
+# # #also the side indices will be returned in order to allow to color them different for example
+# matrix, side_idx = object1.path2matrix_3(k[34][-1])
 # print matrix
-# print side_idx
 # img_with_path = draw_mol_and_path(object1.mol, side_idx)
+
 # img_with_path.show() #unfortunately rdkit forgets to color single c atoms at the moment.
 
 
-# # the paths created can be transformed into path objects, which allows more operations (see the pks_path class)
+# # # the paths created can be transformed into path objects, which allows more operations (see the pks_path class)
 # path_objects = object1.get_pks_paths(k)
 # print path_objects #a list of all the pks_path objects for this pks_mol object
 
